0c6d9ef6d2b2696d19172209b72c92fc4c53e7f5
Shreyansh Lodha <slodha96@gmail.com>
2018-05-03 11:15:28 +0530

    init : Multiple Changes  - Add shell script to put services and daemons to    top-app or system-background  - Make CPU more aggressive and tune interactive  - Enable pDesireAudio by default  - And increase Audio buffer to 1024  - Tune core_ctl  - Update sched params  - Update GPU freq settings as per kernel  - Disable inputboost  - Improve Boot time
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- rootdir/etc/init.oneplus2.power.sh
+++ rootdir/etc/init.oneplus2.power.sh
@@ -43,7 +43,7 @@ write /sys/devices/system/cpu/cpu7/online 0
 restorecon -R /sys/devices/system/cpu
 
 # Best effort limiting for first time boot if msm_performance module is absent
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 960000
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 1248000
 
 # some files in /sysmodule/msm_performance/parameters are created after the restorecon of
 # /sys/. These files receive the default label "sysfs".
@@ -68,52 +68,48 @@ write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor interactive
 restorecon -R /sys/devices/system/cpu # must restore after interactive
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load 1
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay 19000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay 0
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 99
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 19000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 960000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 0
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy 1
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "65 460000:75 960000:80"
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 40000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/powersave_bias 0
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "85 600000:45 672000:50 768000:55 864000:60 960000:70 1248000:85 1478400:95"
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis 80000
-write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 384000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 20000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 60000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_slack 30000
+write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 302400
 
 # configure governor settings for big cluster
 write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor interactive
 restorecon -R /sys/devices/system/cpu # must restore after interactive
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load 1
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay 19000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay "30000 1248000:60000 1728000:30000"
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 99
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 19000
+ write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 30000
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq 1248000
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy 1
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "70 960000:80 1248000:85"
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 40000
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/powersave_bias 1
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "90 1248000:95"
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 30000
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis 80000
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 384000
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 302400
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_slack 30000
 
 # restore A57's max
 copy /sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_max_freq /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
 
-# plugin remaining A57s
-write /sys/devices/system/cpu/cpu5/online 1
-write /sys/devices/system/cpu/cpu6/online 1
-write /sys/devices/system/cpu/cpu7/online 1
-
-# input boost configuration
-write /sys/module/cpu_boost/parameters/input_boost_freq "0:1344000"
-write /sys/module/cpu_boost/parameters/input_boost_ms 40
-
 # Configure core_ctl module parameters
 write /sys/devices/system/cpu/cpu4/core_ctl/max_cpus 4
-write /sys/devices/system/cpu/cpu4/core_ctl/min_cpus 0
-write /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres 60
+write /sys/devices/system/cpu/cpu4/core_ctl/min_cpus 1
+write /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres 50
 write /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres 30
 write /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms  100
 write /sys/devices/system/cpu/cpu4/core_ctl/task_thres 4
 write /sys/devices/system/cpu/cpu0/core_ctl/not_preferred 0
 write /sys/devices/system/cpu/cpu4/core_ctl/is_big_cluster 1
+write /sys/devices/system/cpu/cpu4/core_ctl/always_online_cpu "1 0 0 0" 
 write /sys/devices/system/cpu/cpu0/core_ctl/max_cpus 4
 write /sys/devices/system/cpu/cpu0/core_ctl/min_cpus 4
 write /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres 20
@@ -122,16 +118,26 @@ write /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms 100
 write /sys/devices/system/cpu/cpu0/core_ctl/task_thres 4
 write /sys/devices/system/cpu/cpu0/core_ctl/not_preferred 1
 write /sys/devices/system/cpu/cpu0/core_ctl/is_big_cluster 0
+write /sys/devices/system/cpu/cpu0/core_ctl/always_online_cpu "1 1 1 1"
 chown system:system /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
 chown system:system /sys/devices/system/cpu/cpu4/core_ctl/max_cpus
+
+# plugin remaining A57s
+write /sys/devices/system/cpu/cpu5/online 1
+write /sys/devices/system/cpu/cpu6/online 1
+write /sys/devices/system/cpu/cpu7/online 1
 
 # Setting B.L scheduler parameters
 write /proc/sys/kernel/sched_migration_fixup 1
 write /proc/sys/kernel/sched_small_task 30
-write /proc/sys/kernel/sched_upmigrate 95
-write /proc/sys/kernel/sched_downmigrate 85
+write /proc/sys/kernel/sched_upmigrate 90
+write /proc/sys/kernel/sched_downmigrate 80
+write /proc/sys/kernel/sched_upmigrate_min_nice 8
 write /proc/sys/kernel/sched_freq_inc_notify 400000
 write /proc/sys/kernel/sched_freq_dec_notify 400000
+write /sys/devices/system/cpu/cpu0/sched_mostly_idle_freq 960000
+write /sys/devices/system/cpu/cpu0/sched_mostly_idle_load 25
+write /sys/devices/system/cpu/cpu0/sched_mostly_idle_nr_run 6
 
 # Enable rps static configuration
 write /sys/class/net/rmnet_ipa0/queues/rx-0/rps_cpus 8
@@ -144,11 +150,20 @@ get-set-forall  /sys/class/devfreq/qcom,mincpubw.*/governor cpufreq
 # Disable sched_boost
 write /proc/sys/kernel/sched_boost 0
 
-# change GPU initial power level from 305MHz(level 4) to 180MHz(level 5) for power savings
-write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 5
-
 # set GPU default governor to msm-adreno-tz
 write /sys/class/devfreq/fdb00000.qcom,kgsl-3d0/governor msm-adreno-tz
+write /sys/class/kgsl/kgsl-3d0/devfreq/adrenoboost 0
+write /sys/class/kgsl/kgsl-3d0/max_pwrlevel 1
+write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 8
+write /sys/module/adreno_idler/parameters/adreno_idler_active 1
+write /sys/module/adreno_idler/parameters/adreno_idler_downdifferential 20
+write /sys/module/adreno_idler/parameters/adreno_idler_idlewait 15 
+write /sys/module/adreno_idler/parameters/adreno_idler_idleworkload 3500
+write /sys/class/kgsl/kgsl-3d0/devfreq/min_freq 27000000
+write /sys/class/kgsl/kgsl-3d0/max_gpuclk 510000000
+
+
+write /sys/module/cpu_boost/parameters/input_boost_enabled 0
 
 # re-enable thermal and BCL hotplug
 write /sys/module/msm_thermal/core_control/enabled 1
